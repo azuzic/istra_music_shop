ee3cda7ef944027ca1bfef5acf009c0c5f7ae3dd
azuzic <azuzic@student.unipu.hr>
2021-12-29 15:13:35 +0100

    Initialised branch "početna-stranica"
    - Added logo.svg
    - Created Title.vue component
    - Created test.vue

---
+++ src/components/Title.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="title">
+    <div class="title-bg">
+      <h1 class="title-msg">{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.title {
+  position: absolute;
+  width: 360px;
+  height: 89.5px;
+}
+.title-bg {
+  position: absolute;
+  left: 4.17%;
+  right: 3.89%;
+  top: 13.41%;
+  bottom: 13.97%;
+
+  background: #ff5252;
+  border-radius: 8px;
+}
+
+.title-msg {
+  position: absolute;
+  left: 8.33%;
+  right: 8.33%;
+  top: 32.06%;
+  bottom: 32.19%;
+
+  font-family: Alike;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 23px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #3d3d3f;
+}
+</style>
---
+++ src/views/test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-0 m-0">
+    <img alt="Test logo" src="../assets/logo.svg" />
+    <Title
+      msg="Dobro došli u web aplikaciju 
+      Istra Music Shop-a!"
+    />
+  </div>
+</template>
+
+<script>
+import Title from "@/components/Title.vue";
+
+export default {
+  name: "test",
+  components: {
+    Title,
+  },
+};
+</script>
