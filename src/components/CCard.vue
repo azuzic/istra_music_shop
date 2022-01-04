9a5038c2d49b75b9454ae2de336988c2246358eb
Alesandro Zuzic <azuzic@student.unipu.hr>
2022-01-04 22:34:38 +0100

    Added CCard.vue component

---
+++ src/components/CCard.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="CCard flex">
+    <div class="CCard-image flex-none">
+        <img class="CCard-img mx-auto" :src="img">
+    </div>
+    <div class="CCard-data">
+      <div class="CCard-header">
+        <p class="CCard-title pl-1 pt-1 pb-2 font-bold">
+          {{title}}
+          <b class="CCard-date float-right pr-1">
+            {{date}}
+          </b>
+        </p>
+        <p class="CCard-subtitle pl-1">
+          {{subtitle}}
+          <b class="CCard-price float-right pr-1">
+            {{price}} kn
+          </b>
+        </p>
+      </div>
+      <div class="CCard-footer grid" :class="value!=4 ? 'grid-cols-3':'grid-cols-1'">
+        <div v-if="value!=4" class="CCard-icon-1" :class="value!=1 ? 'tranparent-10':''">
+          <img class="CCard-svg mx-auto mt-2" src="@/assets/deny_icon.svg">
+          <p class="CCard-icontext">ODBIJENO</p>
+        </div>
+        <div v-if="value!=4" class="CCard-icon-2" :class="value!=2 ? 'tranparent-10':''">
+          <img class="CCard-svg mx-auto mt-2" src="@/assets/load_icon.svg">
+          <p class="CCard-icontext">U RAZRADI</p>
+        </div>
+        <div v-if="value!=4" class="CCard-icon-3" :class="value!=3 ? 'tranparent-10':''">
+            <img class="CCard-svg mx-auto mt-2" src="@/assets/accept_icon.svg">
+            <p class="CCard-icontext">PRIHVAĆENO</p>
+        </div>
+        <div v-if="value==4" class="CCard-icon-3">
+            <img class="CCard-svg2 mx-auto mt-1" src="@/assets/deal_icon.svg">
+            <p class="CCard-icontext2">RIJEŠENO</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CCard",
+  props: {
+    value: {
+      type: String,
+      default: "1",
+    },
+    title: {
+      type: String,
+      default: "Title",
+    },
+    subtitle: {
+      type: String,
+      default: "Subtitle",
+    },
+    date: {
+      type: String,
+      default: "Date",
+    },
+    price: {
+      type: String,
+      default: "Price",
+    },
+    img: {
+      type: String,
+      default: "https://picsum.photos/500/500/",
+    },
+  },
+};
+</script>
+<style>
+.CCard {
+  width: 100%;
+  height: 120px;
+  background: #3D3D3F;
+  box-shadow: 0px 0px 2.5px rgba(0, 0, 0, 0.75);
+  border-radius: 8px;
+}
+.CCard-image {
+  width: 120px;
+  height: 120px;
+  background: #3D3D3F;
+  border-bottom-left-radius: 8px;
+  border-top-left-radius: 8px;
+  overflow: hidden;
+}
+.CCard-img {
+  height: 100%;
+  width: auto;
+}
+.CCard-data {
+  width: 100%;
+  height: 120px;
+  background: rgb(150, 164, 165);
+  border-bottom-right-radius: 8px;
+  border-top-right-radius: 8px;
+}
+.CCard-header {
+  width: 100%;
+  height: 48px;
+  background: rgb(255, 255, 255);
+  border-top-right-radius: 8px;
+}
+.CCard-footer {
+  width: 100%;
+  height: 72px;
+  background: #3D3D3F;
+  border-bottom-right-radius: 8px;
+}
+.CCard-icon-1 {
+  height: 100%;
+  background: #FF8080;
+}
+.CCard-icon-2 {
+  height: 100%;
+  background: #FFF383;
+}
+.CCard-icon-3 {
+  height: 100%;
+  background: #6DD277;
+  border-bottom-right-radius: 8px;
+}
+.CCard-icontext {
+  position: relative;
+  text-align: center;
+  font-size: 12px;
+  top: 4px;
+  font-weight: bold;
+  letter-spacing: -1px;
+}
+.CCard-svg {
+  height: 42px;
+  width: 42px;
+  opacity: 75%;
+}
+.CCard-icontext2 {
+  position: relative;
+  text-align: center;
+  font-size: 18px;
+  top: 3px;
+  font-weight: bold;
+  letter-spacing: -1px;
+}
+.CCard-svg2 {
+  height: 42px;
+  width: 42px;
+  opacity: 75%;
+}
+.CCard-title {
+  position: relative;
+  letter-spacing: -1px;
+  font-size: 18px;
+  top: 2px;
+}
+.CCard-subtitle {
+  position: relative;
+  font-size: 11px;
+  letter-spacing: -0.5px;
+  bottom: 2px;
+}
+.CCard-date {
+  position: relative;
+  bottom: 4px;
+  font-size: 12px;
+}
+.CCard-price {
+  position: relative;
+  bottom: 0px;
+  font-size: 12px;
+  color: #e93a3a !important;
+}
+</style>
--- src/views/Djelatnik.vue
+++ src/views/Djelatnik.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!--==============MENU=============================-->
-    <div class="menu grid top-0 grid-cols-2 mb-4">
+    <div class="menu grid grid-cols-2 mb-4">
       <div class="menu-item-active">
         <p>Pregled otkupa</p>
       </div>
@@ -10,8 +10,57 @@
       </div>
     </div>
     <!--==============MENU END=========================-->
+    <!--==============LIST ============================-->
+    <div class="grid grid-rows-auto gap-5 pl-2 pr-2 pb-16 pt-16">
+      <CCard 
+      value="2" 
+      title="Gibson Les Paul" 
+      subtitle="Traditional" 
+      date="20.05.2022." 
+      price="2500"
+      img="https://picsum.photos/500/500/"
+      />
+      <CCard 
+      value="1" 
+      title="Ibanez RG" 
+      subtitle="RG Premium" 
+      date="12.04.2022." 
+      price="800"
+      img="https://picsum.photos/400/400/"
+      />
+      <CCard 
+      value="1" 
+      title="Fender Telecaster" 
+      subtitle="American Standard" 
+      date="29.08.2021." 
+      price="1200"
+      img="https://picsum.photos/600/600/"
+      />
+      <CCard 
+      value="3" 
+      title="Fender Sratocaster" 
+      subtitle="Player Plus Strat" 
+      date="22.05.2022." 
+      price="4500"
+      img="https://picsum.photos/300/300/"
+      />
+      <CCard 
+      value="4" 
+      title="Epiphone SG" 
+      subtitle="Special" 
+      date="01.03.2022." 
+      price="5000"
+      img="https://picsum.photos/700/700/"
+      />
+      <CCard/>
+      <CCard/>
+      <CCard/>
+      <CCard/>
+      <CCard/>
+    </div>
+    <!--==============LIST END=========================-->
     <!--==============FOOTER===========================-->
-    <div class="menu-bottom grid bottom-0 grid-cols-4 mt-4">  
+    <div class="menu-bottom grid grid-cols-4 mt-4">  
       <div class="menu-item">
         <p>U razradi</p>
       </div>
@@ -32,12 +81,14 @@
 <script>
 import CTitle from "@/components/CTitle.vue";
 import CButton from "@/components/CButton.vue";
+import CCard from "@/components/CCard.vue";
 
 export default {
   name: "Djelatnik",
   components: {
     CTitle,
     CButton,
+    CCard,
   },
   methods: {
     dummy() {},
