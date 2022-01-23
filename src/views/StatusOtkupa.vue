d6ad26ef493b8713a66b254b3b4d5c333b80d9a5
lukablaskovic <lblaskovi@unipu.hr>
2022-01-23 20:28:49 +0100

    even listener test again

--- src/views/StatusOtkupa.vue
+++ src/views/StatusOtkupa.vue
@@ -21,9 +21,6 @@ import CCard from "@/components/CCard.vue";
 //Firebase
 import { collection, getDocs } from "@/firebase";
 import { db } from "@/firebase";
-import { query, where, onSnapshot } from "@/firebase";
-
-
 
 export default {
   name: "StatusOtkupa",
@@ -39,17 +36,7 @@ export default {
     CButton,
     CCard,
   },
-  computed:{
-    snapshotHandler(){
-      //Snapshot handler
-      const q = query(collection(db, "zahtjevi"), where("novaPreporucenaCijena", "!=", 0));
-      const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            this.zahtjevi.filter(zahtjev => doc.data().novaPreporucenaCijena).forEach(zahtjev => zahtjev.novaPreporucenaCijena=doc.data().novaPreporucenaCijena);
-        });
-      });
-    }
-  },
+    
   mounted() {
     this.readData();
   },
