4a3649dd989e47f97c5bae0a2a76c1e0c0c107b3
lukablaskovic <lblaskovi@unipu.hr>
2022-01-24 19:42:48 +0100

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -24,6 +24,7 @@ import {
   onSnapshot,
   query,
   where,
+  orderBy,
 } from "firebase/firestore";
 
 import { 
@@ -75,7 +76,8 @@ export {
   updateDoc, 
   where, 
   query, 
-  onSnapshot 
+  onSnapshot,
+  orderBy, 
 };
 //FirebaseStorage exports
 export { 
